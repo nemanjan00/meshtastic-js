ca31428e5c4bc55352b07956e34fe00e995fa9b1
Nemanja Nedeljkovic <nemanjan00@gmail.com>
2024-06-18 23:47:24 +0200

    Interface

---
+++ examples/nodes.js
@@ -0,0 +1,11 @@
+const interfaceFactory = require("../src/interface");
+const commandsFactory = require("../src/commands");
+
+const interface = interfaceFactory("/dev/ttyUSB0");
+
+const commands = commandsFactory(interface);
+
+commands.getNodeDB().then(data => {
+	console.log(data.nodes);
+	process.exit(0);
+});
