e4b3e4f8ad8aecc1d62528b37f6b1f9000a5bed2
Nemanja Nedeljkovic <nemanjan00@gmail.com>
2024-06-18 22:28:00 +0200

    Init

--- examples/node.js
+++ examples/node.js
@@ -11,6 +11,7 @@ modelsRoot.resolvePath = function (_origin, target) {
 const models = modelsRoot.loadSync("meshtastic/mesh.proto")
 
 const ToRadio = models.lookupType("meshtastic.ToRadio");
+const FromRadio = models.lookupType("meshtastic.FromRadio");
 
 //console.log(ToRadio);
 
@@ -19,19 +20,28 @@ const port = new SerialPort({ path: '/dev/ttyUSB0', baudRate: 115200 });
 const START1 = 0x94;
 const START2 = 0xc3;
 
-const packet = ToRadio.create({
-	wantConfigId: 42
-});
-
-console.log(packet);
-const data = ToRadio.encode(packet).finish();
-
-const preamble = Buffer.concat([Buffer.from([START1, START2, 0, 2]), data]);
+const preamble = Buffer.from([START1, START2]);
 
 setInterval(() => {
-	port.write(preamble);
-}, 100);
+	const packet = ToRadio.create({
+		wantConfigId: 42
+	});
+
+	const data = ToRadio.encode(packet).finish();
+
+	const len = new Buffer(2);
+	len.writeInt16BE(data.length);
+
+	port.write(Buffer.concat([preamble, len, data]));
+}, 1000);
 
 port.on("data", data => {
-	console.log(data);
+	console.log(data)
+	const packet = data.slice(4);
+
+	try {
+		console.log(FromRadio.decode(packet));
+	} catch (e) {
+		console.log(e.message);
+	}
 });
