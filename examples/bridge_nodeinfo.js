5cb7d2cfad7a3dcab1563cecaa4bc823623ed730
Nemanja Nedeljkovic <nemanjan00@gmail.com>
2024-07-09 17:43:35 +0200

    Publish decrypted node ingo

--- examples/bridge_nodeinfo.js
+++ examples/bridge_nodeinfo.js
@@ -7,7 +7,7 @@ const client = mqtt.connect(process.env.MQTT_UPSTREAM);
 const clientUpstream = mqtt.connect("mqtt://meshdev:large4cats@mqtt.meshtastic.org");
 
 client.on("connect", () => {
-	client.subscribe("msh/EU_868/2/e/#", (err) => {
+	client.subscribe("msh/EU_868/2/#", (err) => {
 		if(err) {
 			console.error(err);
 		}
@@ -15,40 +15,48 @@ client.on("connect", () => {
 });
 
 client.on("message", (topic, message) => {
-	console.log(topic, message);
+	try {
+		console.log(topic, message);
 
-	const packetContainer = models.ServiceEnvelope.decode(message);
-
-	const packet = packetContainer.packet;
-
-	const keyB64 = "AQ==";
-
-	const handleData = (decrypted) => {
-		const data = models.Data.decode(decrypted);
-
-		console.log(packet);
-		console.log(data);
-
-		if(data.portnum == 1) {
-			console.log(data.payload.toString("utf8"));
+		if(topic.indexOf("map") === -1 && topic.indexOf("/e/") === -1) {
+			return;
 		}
 
-		if(data.portnum == 71 || data.portnum == 73) {
-			packet.decoded = decrypted;
+		const packetContainer = models.ServiceEnvelope.decode(message);
 
-			delete packet.encrypted;
+		const packet = packetContainer.packet;
 
-			clientUpstream.publish(topic, models.ServiceEnvelope.encode(packetContainer).finish());
+		const keyB64 = "AQ==";
+
+		const handleData = (decrypted) => {
+			const data = models.Data.decode(decrypted);
+
+			if(data.portnum == 1) {
+				console.log(data.payload.toString("utf8"));
+			}
+
+			if(data.portnum == 71 || data.portnum == 73) {
+				packet.decoded = decrypted;
+
+				delete packet.encrypted;
+
+				console.log(packet);
+				console.log(data);
+
+				clientUpstream.publish(topic, models.ServiceEnvelope.encode(packetContainer).finish());
+			}
+		};
+
+		if(packet.encrypted) {
+			crypto.decrypt(keyB64, packet).then(decrypted => {
+				handleData(decrypted);
+			}).catch(console.error);
 		}
-	};
 
-	if(packet.encrypted) {
-		crypto.decrypt(keyB64, packet).then(decrypted => {
-			handleData(decrypted);
-		}).catch(console.error);
-	}
-
-	if(packet.decoded) {
-		handleData(packet.decoded);
+		if(packet.decoded) {
+			handleData(packet.decoded);
+		}
+	} catch(e) {
+		console.error(e, topic, message);
 	}
 });
