be9fd9353370c682f971172bdb7412efe3e1a281
Nemanja Nedeljkovic <nemanjan00@gmail.com>
2024-08-21 23:52:13 +0200

    Update

--- examples/timescale.js
+++ examples/timescale.js
@@ -1,12 +1,86 @@
 const pg = require("pg");
+const mqtt = require("mqtt");
+
+const crypto = require("../src/crypto");
+const models = require("../src/models");
 
 pg.types.setTypeParser(pg.types.builtins.INT8, parseInt);
 pg.types.setTypeParser(pg.types.builtins.FLOAT8, parseFloat);
 pg.types.setTypeParser(pg.types.builtins.NUMERIC, parseFloat);
 pg.types.setTypeParser(pg.types.builtins.TIMESTAMP, (date) => new Date(date));
 
+const client = mqtt.connect(process.env.MQTT_UPSTREAM);
+
 const db = require("knex")({
 	client: "pg",
 	connection: process.env.POSTGRES_URL,
 	pool: { min: 0, max: 200 }
+});
+
+const handledPackage = {};
+
+client.on("connect", () => {
+	client.subscribe("msh/EU_868/2/e/MediumFast/#", (err) => {
+		if(err) {
+			console.error(err);
+		}
+	});
+
+	client.on("message", (_topic, packetData) => {
+		const packetContainer = models.ServiceEnvelope.decode(packetData);
+
+		const packet = packetContainer.packet;
+
+		if(handledPackage[packet.id]) {
+			return;
+		}
+
+		handledPackage[packet.id] = true;
+
+		const handleData = (decrypted) => {
+			const data = models.Data.decode(decrypted);
+
+			const dataDecoded = models.Data.toObject(data, {
+				enums: String
+			});
+
+			console.log(dataDecoded, packetContainer)
+
+			db("packets").insert({
+				rx_time: new Date(packetContainer.packet.rxTime * 1000),
+
+				from: packetContainer.packet.from,
+				to: packetContainer.packet.to,
+
+				channel: packetContainer.packet.channel,
+
+				payload: dataDecoded.data || Buffer.from(""),
+				portnum: dataDecoded.portnum,
+
+				id: packetContainer.packet.id,
+
+				rx_snr: packetContainer.packet.rxSnr,
+				rx_rssi: packetContainer.packet.rxRssi,
+
+				hop_limit: packetContainer.packet.hopLimit,
+				hop_start: packetContainer.packet.hopStart,
+
+				channel_id: packetContainer.channelId,
+
+				gateway_id: parseInt(packetContainer.gatewayId.slice(1), 16)
+			}).then(() => {});
+		};
+
+		const keyB64 = "AQ==";
+
+		if(packet.encrypted) {
+			crypto.decrypt(keyB64, packet).then(decrypted => {
+				handleData(decrypted);
+			}).catch(console.error);
+		}
+
+		if(packet.decoded) {
+			handleData(packet.decoded);
+		}
+	});
 });
--- migrations/20240821205529_packets.js
+++ migrations/20240821205529_packets.js
@@ -2,25 +2,25 @@ exports.up = function(knex) {
 	return knex.schema.createTable("packets", function(table) {
 		table.timestamp("rx_time").defaultTo(knex.fn.now()).notNullable();
 
-		table.integer("from").notNullable();
-		table.integer("to").notNullable();
+		table.bigint("from").notNullable();
+		table.bigint("to").notNullable();
 
-		table.integer("channel").notNullable();
+		table.bigint("channel").notNullable();
 
 		table.binary("payload", 256).notNullable();
 		table.string("portnum", 50).notNullable();
 
-		table.integer("id").notNullable();
+		table.bigint("id").notNullable();
 
-		table.integer("rx_snr").notNullable();
-		table.integer("rx_rssi").notNullable();
+		table.float("rx_snr").notNullable();
+		table.float("rx_rssi").notNullable();
 
 		table.integer("hop_limit").notNullable();
 		table.integer("hop_start").notNullable();
 
 		table.string("channel_id").notNullable();
 
-		table.integer("gateway_id").notNullable();
+		table.bigint("gateway_id").notNullable();
 	}).then(() => {
 		return knex.raw("SELECT create_hypertable('packets', 'rx_time');");
 	});
