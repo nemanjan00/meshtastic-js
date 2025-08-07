a2b1e95027e6f66ffb8c85714bb3cd3db69fb569
Nemanja Nedeljkovic <nemanjan00@gmail.com>
2025-08-07 15:33:58 +0200

    Sync

--- examples/nodeinfo.js
+++ examples/nodeinfo.js
@@ -239,7 +239,7 @@ client.on("message", (topic, message) => {
 
 		if(db[packet.from] === undefined) {
 			setTimeout(() => {
-				sendMessage(`Dobro dosli na Meshtastic Srbija (${packet.from})`);
+				sendMessage(`Dobro dosli na Meshtastic Srbija (${packet.from.toString(16).padStart(6, "0")})`);
 				sendMessage("Welcome to Meshtastic Serbia");
 				sendMessage("Telegram: https://t.me/meshtasticsrb\n\nDocumentation: https://shorturl.at/PihU6");
 			}, 60000);
