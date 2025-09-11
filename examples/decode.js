e39099d30502c10f6cc11fa325f6c9cc0a85e624
Nemanja Nedeljkovic <nemanjan00@gmail.com>
2025-09-11 21:27:37 +0200

    Handle errors

--- examples/decode.js
+++ examples/decode.js
@@ -5,36 +5,42 @@ const readline = require("readline");
 const data = Buffer.from("FFFFFFFF6DE3CE51EDCAC8AD631F006D24566D2A6F94A1D91DAD".split(" ").join(""), "hex");
 
 const print = data => {
-	const packet = {
-		to: data.readUint32LE(0),
-		from: data.readUint32LE(4),
-		id: data.readUint32LE(8),
-		flagsByte: data.readUint8(12),
-		channel: data.readUint8(13),
-		nextHop: data.readUint8(14),
-		relayNode: data.readUint8(15),
-		encrypted: data.slice(16)
-	};
+	try {
+		const packet = {
+			to: data.readUint32LE(0),
+			from: data.readUint32LE(4),
+			id: data.readUint32LE(8),
+			flagsByte: data.readUint8(12),
+			channel: data.readUint8(13),
+			nextHop: data.readUint8(14),
+			relayNode: data.readUint8(15),
+			encrypted: data.slice(16)
+		};
 
 
-	packet.flags = {};
+		packet.flags = {};
 
-	packet.flags.hopLimit = packet.flagsByte & 0x07;
-	packet.flags.wantAck = (packet.flagsByte & 0x08) >> 3;
-	packet.flags.viaMqtt = (packet.flagsByte & 0x10) >> 4;
-	packet.flags.hopStart = (packet.flagsByte & 0xE0) >> 5;
+		packet.flags.hopLimit = packet.flagsByte & 0x07;
+		packet.flags.wantAck = (packet.flagsByte & 0x08) >> 3;
+		packet.flags.viaMqtt = (packet.flagsByte & 0x10) >> 4;
+		packet.flags.hopStart = (packet.flagsByte & 0xE0) >> 5;
 
-	const keyB64 = "AQ==";
+		const keyB64 = "AQ==";
 
-	crypto.decrypt(keyB64, packet).then(data => {
-		const decoded = models.Data.decode(data);
+		crypto.decrypt(keyB64, packet).then(data => {
+			const decoded = models.Data.decode(data);
 
-		const dataDecoded = models.Data.toObject(decoded, {
-			enums: String
+			const dataDecoded = models.Data.toObject(decoded, {
+				enums: String
+			});
+
+			console.log(packet, dataDecoded);
+		}).catch(() => {
+			console.log(packet, "Not decrypted");
 		});
-
-		console.log(packet, dataDecoded);
-	});
+	} catch (error) {
+		console.error(`Failed to decode ${line}`);
+	}
 };
 
 //print(data);
